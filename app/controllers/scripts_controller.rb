c29fa97b90387582f42bfcac8d9324865be22487
Jamison Bunge <JamisonBunge@outlook.com>
2020-03-01 14:49:32 -0500

    set up more routes:

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,12 +1,29 @@
 class ScriptsController < ApplicationController
+  def script_params
+    params.require(:script).permit(:title, :author)
+  end
 
   def index
     dummy_email = 'ajd2215' #this will be handled by session[] when user auth is up
     @scripts = Script.for_user(dummy_email)
 
   end
-  def show
-  @script_id = params[:id]
 
+  def show
+  script_id = params[:id]
+  @script = Script.find(script_id)
+
+  end
+
+  def new
+    # renders new template
+
+  end
+
+  def create
+    dummy_email = 'ajd2215'
+    @script = Script.add_new(script_params,dummy_email)
+    flash[:notice] = "#{@script.title} was succesfully created."
+    redirect_to scripts_path
   end
 end
--- app/models/script.rb
+++ app/models/script.rb
@@ -2,4 +2,8 @@ class Script < ApplicationRecord
     def self.for_user(email)
         Script.where(email: email)
     end
+
+    def self.add_new(script_params,email)
+        Script.create!(script_params.merge({email:email}))
+    end
 end
