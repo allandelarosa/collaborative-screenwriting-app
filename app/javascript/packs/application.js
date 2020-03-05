0b3d174cf77af84fd08498ed1aebc06c320fb96b
Jamison Bunge <JamisonBunge@outlook.com>
2020-03-04 22:42:02 -0500

    added text editor, messed around with adding things to the asset pipeline, updated scripts index view, messed around with some css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("packs/editor")
+// require("packs/editor_config")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/packs/editor_config.js
@@ -0,0 +1,9 @@
+
+
+const editor = new EditorJS({
+    /**
+    * Id of Element that should contai
+    * dn Editor instance
+    */
+    holderId: 'editorjs'
+});
