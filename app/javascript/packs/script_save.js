9d86c344f22eff8d739b37529cc9e81b01df455d
Allan Delarosa <ajd2215@columbia.edu>
2020-05-03 03:19:46 -0400

    Some error checking for script title and author; not sure if this is browser dependent

--- app/javascript/packs/script_save.js
+++ app/javascript/packs/script_save.js
@@ -6,7 +6,12 @@ async function save_script(e) {
     let script_title = document.getElementById('script_title').innerHTML
     let script_author = document.getElementById('script_author').innerHTML
 
-    console.log(scriptPath)
+    // prevent user from creating html elements
+    // also gets rid of weird special characters
+    // ALSO get rid of newlines, in case that somehow gets in there
+    script_title = script_title.replace(/(<.*?>)|(&.+?;)|([\r\n\v]+)/g, '').trim()
+    script_author = script_author.replace(/(<.*?>)|(&.+?;)|([\r\n\v]+)/g, '').trim()
+
     console.log(script_title)
     console.log(script_author)
 
@@ -16,8 +21,6 @@ async function save_script(e) {
         data: { title: script_title, author: script_author, save_script: 1 },
         beforeSend: function (xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) },
         success: function (data, textStatus, jqXHR) {
-
-            //alert("the file has been saved!")
             console.log("Allan's-script-save-passed")
         },
         error: function (jqXHR, textStatus, errorThrown) { console.log("Allan's-script-save-failed") }
@@ -27,5 +30,5 @@ async function save_script(e) {
 var timer = null;
 $('#script_title,#script_author').keydown(function () {
     clearTimeout(timer);
-    timer = setTimeout(save_script, 1000)
+    timer = setTimeout(save_script, 2000)
 });
